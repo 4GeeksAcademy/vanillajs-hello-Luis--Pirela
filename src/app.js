3ab887f536b21214f692cc9cff8de4d660de5a40
Pirela47 <luis.jpl294@gmail.com>
2024-09-01 19:07:06 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let whordm = Math.floor(Math.random() * who.length);
+let actionrdm = Math.floor(Math.random() * action.length);
+let whatrdm = Math.floor(Math.random() * what.length);
+let whenrdm = Math.floor(Math.random() * when.length);
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const p = document.getElementById("excuse");
+  p.innerText =
+    who[whordm] +
+    " " +
+    action[actionrdm] +
+    " " +
+    what[whatrdm] +
+    " " +
+    when[whenrdm];
 };
